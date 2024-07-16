8805f0ee96a92753757823575735fd285c2e4006
YeoJinSoo <yjs6940@naver.com>
2024-07-16 11:53:18 +0900

    Git 학습용 스프링 프로젝트 생성

---
+++ src/main/java/com/ictedu/springgit/SpringGitProApplication.java
@@ -0,0 +1,13 @@
+package com.ictedu.springgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ictedu/springgit/SpringGitProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ictedu.springgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
